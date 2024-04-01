64445feeb11f30ca058754a12aa9044d8df66b13
chakAs3 <javachakir@gmail.com>
2024-04-01 18:14:18 +0400

    add pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
+  pinia: {
+    autoImports: ['defineStore', 'acceptHMRUpdate'],
+  },
+  imports: {
+    dirs: ['./stores'],
+  },
 })
